30c20e98c2b7ef4706691419c0753eac8bb06836
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2018-03-13 10:29:39 +1100

    Missing reference to Jupyter notebook file for service.

--- .jupyter/jupyter_kernel_gateway_config.py
+++ .jupyter/jupyter_kernel_gateway_config.py
@@ -1,3 +1,4 @@
 c.KernelGatewayApp.api = 'kernel_gateway.notebook_http'
 c.KernelGatewayApp.ip = '0.0.0.0'
 c.KernelGatewayApp.port = 8080
+c.KernelGatewayApp.seed_uri = '/opt/app-root/src/Airports.ipynb'
